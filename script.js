eadfc8506ad70b97a7742b155fddedfcafa4a9f1
ProjTroy <tmbryant56@gmail.com>
2022-07-06 11:45:35 -0700

    Finished base game

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let playerSelection = "";
+let computerSelection = computerPlay();
+
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -20,21 +26,24 @@ function playRound(playerSelection, computerSelection) {
         alert ("It's a draw!");
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         alert ("You Lose! Paper beats Rock");
+        computerScore++;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         alert ("You Win! Rock beats Scissors");
+        playerScore++;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         alert ("You Win! Paper beats Rock");
+        playerScore++;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         alert ("You Lose! Scissors beats Paper");
+        computerScore++;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         alert ("You Lose! Rock beats scissors");
+        computerScore++;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         alert ("You Win! Scissors beats Paper");
+        playerScore++;
     }
 }
-
-let playerSelection = "";
-let computerSelection = computerPlay();
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -43,17 +52,47 @@ buttons.forEach((button) => {
             computerSelection = computerPlay();
             playerSelection = "rock";
             playRound(playerSelection, computerSelection);
+            updateScore();
             console.log("computer: " + computerSelection);
         } else if (button.id == "paper") {
             computerSelection = computerPlay();
             playerSelection = "paper";
             playRound(playerSelection, computerSelection);
+            updateScore();
             console.log("computer: " + computerSelection);
         } else if (button.id == "scissors") {
             computerSelection = computerPlay();
             playerSelection = "scissors";
             playRound(playerSelection, computerSelection);
+            updateScore();
             console.log("computer: " + computerSelection);
         }
     });
+});
+
+function updateScore() {
+    const pScore = document.getElementById("playerScore");
+    const cScore = document.getElementById("computerScore");
+
+    if (playerScore == 5) {
+        pScore.textContent = `Player Wins!`;
+    } else if (computerScore == 5) {
+        cScore.textContent = `Computer Wins!`;
+    } else if (playerScore < 5 || computerScore < 5) {
+        pScore.textContent = `Player: ${playerScore}`;
+        cScore.textContent = `Computer: ${computerScore}`;
+    }
+    
+}
+
+function resetGame() {
+    computerScore = 0;
+    playerScore = 0;
+    pScore.textContent = `Player: ${playerScore}`;
+    cScore.textContent = `Computer: ${computerScore}`;
+}
+
+const restartButton = document.querySelector('#restart-button');
+restartButton.addEventListener('click', () => {
+    resetGame();
 });
